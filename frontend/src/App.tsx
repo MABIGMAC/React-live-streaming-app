50389c48edaf3b91e63ab7a59d0c4b14b13eac57
McBigMac <44628096+MABIGMAC@users.noreply.github.com>
2025-04-03 16:57:37 +0800

    feat: create hls and bind to video

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,37 @@
 import { Button } from 'antd'
-
+import Hls from 'hls.js'
+import { useEffect, useRef } from 'react'
+/* eslint-disable  @typescript-eslint/no-unsafe-call*/
+/* eslint-disable  @typescript-eslint/no-unsafe-member-access*/
 function App() {
+	const videoRef = useRef<HTMLVideoElement | null>(null)
+
+	useEffect(() => {
+		const video = videoRef.current
+
+		if (!video) return
+
+		if (
+			Hls.isSupported() &&
+			window.MediaSource?.isTypeSupported('video/mp4;codecs="av01.0.01M.08"')
+		) {
+			const hls = new Hls()
+
+			hls.attachMedia(video)
+			hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+				console.log('video and hls.js are now bound together!')
+			})
+
+			return () => {
+				hls.destroy() // Cleanup on unmount
+			}
+		}
+	}, [])
+
 	return (
 		<div className="App">
 			<Button type="primary">Button</Button>
+			<video ref={videoRef} controls></video>
 		</div>
 	)
 }
